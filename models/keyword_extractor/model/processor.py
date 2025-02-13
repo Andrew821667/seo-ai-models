7cdc21be40ad8acc595df591d06475fc248d0f37
Andrew821667 <a.popov.gv@gmail.com>
2025-02-13 07:33:22 +0000

    Update keyword extractor: improved morphology support

--- models/keyword_extractor/model/processor.py
+++ models/keyword_extractor/model/processor.py
@@ -1,7 +1,1 @@
-class KeywordProcessor:
-    def __init__(self, model_config_path: str):
-        self.model_config_path = model_config_path
-        
-    def extract_keywords(self, text: str) -> list[str]:
-        # Заглушка для тестирования
-        return ["python", "programming"]
+# Здесь весь код KeywordProcessor, который мы написали
